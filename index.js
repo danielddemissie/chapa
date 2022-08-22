ecfc06748dfb08c7e3fe67d59b4738dbcb44d0d8
daniel <danielddemissie@gmail.com>
2022-08-23 00:59:11 +0300

    fix: node-fetch

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch');
-const request = require('request');
 const initialize_url = 'https://api.chapa.co/v1/transaction/initialize';
 const verify_url = 'https://api.chapa.co/v1/transaction/verify/';
 
@@ -8,62 +7,62 @@ const verify_url = 'https://api.chapa.co/v1/transaction/verify/';
  * @param {string} chapaKey
  */
 function Chapa(chapaKey) {
-  this.chapaKey = chapaKey;
-}
+  var chapaKey = chapaKey;
 
-/**
- * 
- * @param {Object} initializeInfo customer information and customization
- * @returns {Promise}
- */
-Chapa.prototype.initialize = function (initializeInfo) {
-  const requiredParams = ['email','amount','first_name','last_name','tx_ref','currency'];
-  let missingParams = [];
-  missingParams = requiredParams.filter((key)=>!object.hasOwnProperty(key))
-  if(missingParams.length>0) {
-    throw new Error(`The initializeInfo has ${missingParams.length} missing required paramater '${[...missingParams]}'`)}
-
-  return new Promise((resolve, reject) => {
-    fetch(initialize_url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + this.chapaKey,
-      },
-      body:JSON.stringify(initializeInfo)
-    }).then(async(res)=>{
-      if(res.status===200){
-        resolve(await res.json())
-      }
-    }).catch((error)=>{
-      reject(error)
+  /**
+   * 
+   * @param {object} initializeInfo customer information and customization
+   * @returns {Promise}
+   */
+  this.initialize = function (initializeInfo) {
+    const requiredParams = ['email','amount','first_name','last_name','tx_ref','currency'];
+    let missingParams = [];
+    missingParams = requiredParams.filter((key)=>!object.hasOwnProperty(key))
+    if(missingParams.length>0) {
+      throw new Error(`The initializeInfo has ${missingParams.length} missing required paramater '${[...missingParams]}'`)
+    }  
+    return new Promise((resolve, reject) => {
+      fetch(initialize_url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + chapaKey,
+        },
+        body:JSON.stringify(initializeInfo)
+      }).then(async(res)=>{
+        if(res.status===200){
+          resolve(await res.json())
+        }
+      }).catch((error)=>{
+        reject(error)
+      })
+    });
+  };
+  
+  
+  /**
+   * 
+   * @param {string} tnxRef 
+   * @returns {Promise}
+   */
+  this.verify = function(tnxRef){
+    if(!tnxRef) throw new Error('Transaction refrence is required!')
+    return new Promise((resolve,reject)=>{
+      fetch(verify_url+tnxRef, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + chapaKey,
+        }
+      }).then(async(res)=>{
+        if(res.status===200){
+          resolve(await res.json())
+        }
+      }).catch((error)=>{
+        reject(error)
+      })
     })
-  });
-};
-
-
-/**
- * 
- * @param {string} tnxRef 
- * @returns {Promise}
- */
-Chapa.prototype.verify = function(tnxRef){
-  if(!tnxRef) throw new Error('Transaction refrence is required!')
-  return new Promise((resolve,reject)=>{
-    fetch(verify_url+tnxRef, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + this.chapaKey,
-      }
-    }).then(async(res)=>{
-      if(res.status===200){
-        resolve(await res.json())
-      }
-    }).catch((error)=>{
-      reject(error)
-    })
-  })
+  }
 }
 
 module.exports = Chapa;
