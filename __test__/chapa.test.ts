eef281157e41639584ca47bb2e166a2c3dcff784
Daniel Demelash <danielddemissie@gmail.com>
2023-10-12 15:47:37 +0300

    fix: update

--- __test__/chapa.test.ts
+++ __test__/chapa.test.ts
@@ -28,43 +28,24 @@ describe('Chapa test-cases', () => {
     });
   });
 
-  it('iInitialize fn resolve with correct data ', () => {
-    const initializeInfo = chapa.initialize(
-      {
-        email: 'daniel@daniel.com',
-        amount: 100,
-        first_name: 'daniel',
-        last_name: 'demelash',
-        currency: 'ETB',
-        callback_url: 'http://localhost:3000',
-      },
-      { autoTx_ref: true },
-    );
+  it('Initialize fn resolve with correct data ', () => {
+    const initializeInfo = chapa.initialize({
+      email: 'daniel@daniel.com',
+      amount: 100,
+      first_name: 'daniel',
+      last_name: 'demelash',
+      currency: 'ETB',
+      callback_url: 'http://localhost:3000',
+      tx_ref: chapa.generateTxRef(),
+    });
     return expect(initializeInfo).resolves.not.toThrow();
   });
 
-  it('verify fn rejects with wrong tx_ref', () => {
+  it('Verify fn rejects with wrong tx_ref', () => {
     const verify = chapa.verify('tx_ref12');
 
     return expect(verify).rejects.not.toThrow();
   });
-
-  // it('transfer fn reject insufficent balance with transfer info', () => {
-  //   const transferInfo = chapa.transfer({
-  //     account_name: 'daniel',
-  //     account_number: '12345678',
-  //     beneficiary_name: 'shaka',
-  //     reference: 'refrence',
-  //     bank_code: '32735b19-bb36-4cd7-b226-fb7451cd98f0',
-  //     amount: 100,
-  //     currency: 'ETB',
-  //   });
-  //   return expect(transferInfo).rejects.toStrictEqual({
-  //     data: null,
-  //     message: 'Insufficient Balalnce',
-  //     status: 'failed',
-  //   });
-  // });
 
   it('get all banks info', () => {
     const allBanks = chapa.getBanks();
@@ -72,20 +53,20 @@ describe('Chapa test-cases', () => {
     return expect(allBanks).resolves.not.toThrow();
   });
 
-  // it('Create subaccout with subaccount info', () => {
-  //   const subaccount = chapa.createSubAccount({
-  //     bank_code: '32735b19-bb36-4cd7-b226-fb7451cd98f0',
-  //     business_name: 'shaka',
-  //     account_name: 'shaka',
-  //     account_number: '12345678',
-  //     split_type: 'flat',
-  //     split_value: 2,
-  //   });
+  it('Create subaccout with subaccount info', () => {
+    const subaccount = chapa.createSubAccount({
+      bank_code: '32735b19-bb36-4cd7-b226-fb7451cd98f0',
+      business_name: 'shaka',
+      account_name: 'shaka',
+      account_number: '12345678',
+      split_type: 'flat',
+      split_value: 2,
+    });
 
-  //   return expect(subaccount).rejects.toStrictEqual({
-  //     data: null,
-  //     message: 'To create subaccounts via API you need to be on live mode.',
-  //     status: 'failed',
-  //   });
-  // });
+    return expect(subaccount).rejects.toStrictEqual({
+      data: null,
+      message: 'This subaccount does exist',
+      status: 'failed',
+    });
+  });
 });
